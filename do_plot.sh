7d08eab57973b976936b50fef01f0da5b24e6b81
Josh Koike <jk@tilting.me>
2020-06-25 16:16:13 -0700

    Add gitlab CI runner

---
+++ do_plot.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH="/usr/lib/kicad-nightly/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH"
+export PYTHONPATH="/usr/lib/kicad-nightly/lib/python3/dist-packages/:$PYTHONPATH"
+python kicadScripts/plot_gerbers.py hub75_poe.kicad_pcb $(git rev-parse --short HEAD)
